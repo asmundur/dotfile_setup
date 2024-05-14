b7c201b5621e12a59bc7665921ed6a79977d75f7
asmundur <asmundurg@gmail.com>
2024-05-14 17:27:45 +0000

    setup_new_machine.sh added

---
+++ setup_new_machine.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Clone the dotfiles repository as a bare repository
+git clone --bare git@github.com:asmundur/dotfiles.git $HOME/.cfg
+
+# Define the 'config' function to manage dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+# Create a backup directory for any pre-existing dotfiles
+mkdir -p .config-backup
+
+# Attempt to check out the dotfiles
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config."
+else
+  echo "Backing up pre-existing dot files."
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  config checkout
+fi
+
+# Configure Git to ignore untracked files
+config config status.showUntrackedFiles no
